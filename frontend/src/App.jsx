da546b9c7efd1072ee6a8701acbeff0db5801607
Chirapat <chirapat.ch@ku.th>
2025-10-22 13:14:45 +0700

    INITIAL: Setup React

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1>Hotel Reservation Web Application</h1>
+  )
+}
+
+export default App
